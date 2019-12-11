0da070c5625b611349425033757ed07bfc356577
eugenesqr <eskurihin@gmail.com>
2019-12-11 23:49:54 +0300

    initial web commit

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import fetch from 'cross-fetch';
+import 'here-js-api/scripts/mapsjs-core';
+import 'here-js-api/scripts/mapsjs-service';
 import './styles.less';
 
-console.log('hello world');
+fetch('//ip-api.com/json').then(function(result) {
+  return result.json();
+}).then(function(res) {
+  console.log(res);
+});
+ReactDOM.render(
+  (<React.Fragment>
+    <h1>I AM HERE!</h1>
+  </React.Fragment>),
+  document.getElementById('root'),
+);
