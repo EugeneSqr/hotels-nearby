d4e89d94715da570d4db44b6e28bb60d0b28ed8d
eugenesqr <eskurihin@gmail.com>
2019-12-14 17:08:41 +0300

    make hotelRepository work with networkService

--- service/hotelRepository.js
+++ service/hotelRepository.js
@@ -1,10 +1,12 @@
 'use strict';
+const networkService = require('./networkService');
 /**
  * Responsible for getting hotels information either from network or from
  * database cache
  */
 module.exports = {
-  getHotels: function() {
+  getHotels: function(latitude, longitude, radius) {
+    return networkService.getHotels(latitude, longitude, radius);
   },
 
   getHotelDetails: function() {
